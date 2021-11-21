471eaf097d956c2c6081e9aad4c712839f273412
Ederson Cascaes <edercascaes@gmail.com>
2021-11-21 17:04:58 -0300

    third commit

---
+++ AllService.WebApi_/Controllers/PrestadorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AllService.WebApi_.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PrestadorController : ControllerBase
+    {
+        
+          //todo Ilogger
+        public PrestadorController()
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar()
+        {
+            return Ok("Cadastro Realizado com sucesso!");
+        }
+
+    }
+}
